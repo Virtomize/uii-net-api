10d0b8fc50999695245f021b75d2cc9d9bd4fa39
Frank Taubert <frank.taubert@uni-jena.de>
2022-07-24 16:53:53 +0100

    update example

--- UsageExample/Program.cs
+++ UsageExample/Program.cs
@@ -1,25 +1,25 @@
-﻿//using UII;
+﻿using UII;
 
-//var client = new UII.Client("Token here");
+var client = new UII.Client("Token here");
 
-//var osList = await client.ReadOsList();
-//Console.WriteLine("Supported operations systems:");
-//Console.WriteLine(string.Join('\n',osList.Select(os => os.DisplayName)));
+var osList = await client.ReadOsList();
+Console.WriteLine("Supported operations systems:");
+Console.WriteLine(string.Join('\n',osList.Select(os => os.DisplayName)));
 
-//var packageList = await client.ReadPackageList("debian", "10", "x86_64");
-//Console.WriteLine($"Found {packageList.Packages.Count()} packages for {packageList.Distribution} version {packageList.Version}");
+var packageList = await client.ReadPackageList("debian", "10", "x86_64");
+Console.WriteLine($"Found {packageList.Packages.Count()} packages for {packageList.Distribution} version {packageList.Version}");
 
-//await client.Build(
-//    "c:/tmp/debian.iso",
-//    "debian",
-//    "10",
-//    "x86_64",
-//    "hostName" ,
-//    new List<Network>
-//    {
-//        new ()
-//        {
-//            HasNoInternet = false,
-//            UsesDhcp = true,
-//        },
-//    });
+await client.Build(
+    "c:/tmp/debian.iso",
+    "debian",
+    "10",
+    "x86_64",
+    "hostName" ,
+    new List<Network>
+    {
+        new ()
+        {
+            HasNoInternet = false,
+            UsesDhcp = true,
+        },
+    });
